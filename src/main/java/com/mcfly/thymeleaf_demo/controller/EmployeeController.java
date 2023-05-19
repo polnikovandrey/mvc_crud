164a48f4c2c22f377e581da1c779fd5ddc4e2dcc
polnikov.andrey <polnikov.andrey1981@gmail.com>
2023-05-19 18:31:53 +0300

    + get employees from the db

--- src/main/java/com/mcfly/thymeleaf_demo/controller/EmployeeController.java
+++ src/main/java/com/mcfly/thymeleaf_demo/controller/EmployeeController.java
@@ -1,45 +1,30 @@
 package com.mcfly.thymeleaf_demo.controller;
 
 import com.mcfly.thymeleaf_demo.entity.Employee;
-import jakarta.annotation.PostConstruct;
+import com.mcfly.thymeleaf_demo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
 
-	// load employee data
+	private final EmployeeService employeeService;
 
-	private List<Employee> theEmployees;
-
-	@PostConstruct
-	private void loadData() {
-
-		Employee emp1 = new Employee("Leslie", "Andrews", "leslie@luv2code.com");
-		Employee emp2 = new Employee("Emma", "Baumgarten", "emma@luv2code.com");
-		Employee emp3 = new Employee("Avani", "Gupta", "avani@luv2code.com");
-
-		theEmployees = new ArrayList<>();
-
-		theEmployees.add(emp1);
-		theEmployees.add(emp2);
-		theEmployees.add(emp3);
+	@Autowired
+	public EmployeeController(EmployeeService employeeService) {
+		this.employeeService = employeeService;
 	}
 
-	// add mapping for "/list"
-
 	@GetMapping("/list")
-	public String listEmployees(Model theModel) {
-
-		// add to the spring model
-		theModel.addAttribute("employees", theEmployees);
-
+	public String listEmployees(Model model) {
+		final List<Employee> employees = employeeService.findAll();
+		model.addAttribute("employees", employees);
 		return "list-employees";
 	}
 }
